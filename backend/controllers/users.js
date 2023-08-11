f90c1e31efd2f860e79eca51b592dd1656c779ac
lliikko <angelikayangirova@gmail.com>
2023-08-11 22:52:35 +0500

    1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -64,7 +64,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
         expiresIn: '7d',
       });
-
+      print('text', JWT_SECRET);
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
