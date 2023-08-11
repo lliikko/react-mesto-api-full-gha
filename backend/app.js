98c373f26de1812197cd271f729868d49fb235d9
lliikko <angelikayangirova@gmail.com>
2023-08-11 18:47:00 +0500

    4

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found');
 
-const { PORT = 5000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 // подключаемся к серверу mongo
