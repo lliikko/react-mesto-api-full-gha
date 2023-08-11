6b9baaf6ed9c971de63a0f5b9fc0ea1af8b3ce97
lliikko <angelikayangirova@gmail.com>
2023-08-11 18:41:08 +0500

    try2

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://angelikayang.nomoreparties.co', credentials: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(requestLogger);
