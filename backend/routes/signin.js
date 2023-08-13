a2bbff8d2f90c329a70bd169853aec8a0ba6d950
lliikko <angelikayangirova@gmail.com>
2023-08-13 18:37:08 +0500

    777

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -6,7 +6,7 @@ const { login, signout } = require('../controllers/users');
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 }), login);
 router.get('/signout', signout);
